a736860c292cb02f4f96b1393f63b5f6e362b248
paarmin <parniaa@gmail.com>
2021-02-19 15:34:21 -0500

    w7D3 one left:

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,22 +47,21 @@ const myMappedApp = appointments.map((app) => {
 });
 
 export default function Application(props) {
-  // const [days, setDays] = useState([]);
-  // const [day, setDay] = useState("Monday");
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {}
   });
-  
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
+
+
 
   useEffect(() => {
     const url = "http://localhost:8001/api/days";
     axios.get(url).then((response) => {
-      setState.days(response.data);
-      // console.log("this is the res data", response.data);
+      setDays(response.data);
     });
   }, []);
 
@@ -76,13 +75,11 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-
-          <DayList
-    days={state.days}
-    day={state.day}
-    setDay={setState.day}
-/>
-
+          <DayList 
+            days={state.days} 
+            day={state.day} 
+            setDay = {setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
